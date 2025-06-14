37c71b64832054a24374daf9816dc5de28ee5e01
George-Markas <georgemarkanton@proton.me>
2025-06-14 21:28:00 +0300

    Update

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,36 +1,26 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 5};
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft  = 0;   /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 3;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Iosevka:size=14:antialias=true:autohint=true", "NotoColorEmoji:size=14:antialias=true:autohint=true" };
+static const char *fonts[]          = { "Iosevka:size=14:antialias=true:autohint=true",
+                                        "NotoColorEmoji:size=14:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=10";
 
 static const char normbgcolor[]     = "#282828";
-static const char normfgcolor[]     = "#928374"; 
-static const char normbordercolor[] = "#585858";
+static const char normfgcolor[]     = "#a89984";
+static const char normbordercolor[] = "#444444";
 static const char selbgcolor[]      = "#3c3836";
-static const char selfgcolor[]      = "#d79921";
-static const char selbordercolor[]  = "#928374";
+static const char selfgcolor[]      = "#fbf1c7";
+static const char selbordercolor[]  = "#d79921";
 
 static const char *colors[][3]      = {
-    /*                   fg           bg           border   */
-    [SchemeNorm]     = { normfgcolor, normbgcolor, normbordercolor },
-    [SchemeSel]      = { selfgcolor,  selbgcolor,  selbordercolor  },
-    [SchemeStatus]   = { "#c8c09f",   normbgcolor, "#000000"       }, // Statusbar right {text, background, not used but cannot be empty}
-    [SchemeTagsSel]  = { selfgcolor,  selbgcolor,  "#000000"       }, // Tagbar left selected {text, background, not used but cannot be empty}
-    [SchemeTagsNorm] = { normfgcolor, normbgcolor, "#000000"       }, // Tagbar left unselected {text, background, not used but cannot be empty}
-    [SchemeInfoSel]  = { "#c8c09f",   selbgcolor,  "#000000"       }, // Infobar middle selected {text, background, not used but cannot be empty}
-    [SchemeInfoNorm] = { normfgcolor, normbgcolor, "#000000"       }, // Infobar middle unselected {text, background, not used but cannot be empty}
+    /*               fg           bg           border   */
+    [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+    [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
 /* tagging */
@@ -41,8 +31,10 @@ static const Rule rules[] = {
      *  WM_CLASS(STRING) = instance, class
      *  WM_NAME(STRING) = title
      */
-    
+
     /* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+    { "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
+    { "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
     { "st",      NULL,     NULL,           0,         0,          1,           0,        -1 },
     { NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
@@ -75,7 +67,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
-static const char *roficmd[] = { "rofi", "-show", "drun", NULL };
+static const char *roficmd[]  = { "rofi", "-show", "drun", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 #include <X11/XF86keysym.h>
@@ -88,8 +80,8 @@ static const Key keys[] = {
     { MODKEY,                       XK_b,      togglebar,      {0} },
     { MODKEY,                       XK_Right,  focusstack,     {.i = +1 } },
     { MODKEY,                       XK_Left,   focusstack,     {.i = -1 } },
-    { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-    { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+    { ControlMask,                  XK_i,      incnmaster,     {.i = +1 } },
+    { ControlMask,                  XK_d,      incnmaster,     {.i = -1 } },
     { ControlMask,                  XK_Left,   setmfact,       {.f = -0.05} },
     { ControlMask,                  XK_Right,  setmfact,       {.f = +0.05} },
     { MODKEY|ShiftMask,             XK_Left,   movestack,      {.i = +1 } },
@@ -97,9 +89,9 @@ static const Key keys[] = {
     //{ MODKEY,                       XK_Return, zoom,           {0} },
     { MODKEY,                       XK_Tab,    view,           {0} },
     { MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
-    //{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-    //{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-    //{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+    { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+    { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+    { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
     { MODKEY,                       XK_space,  setlayout,      {0} },
     { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
     { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -108,10 +100,6 @@ static const Key keys[] = {
     { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
     { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
     { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-    { MODKEY,                       XK_minus,  setgaps,        {.i = -5 } },
-    { MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
-    { MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = GAP_RESET } },
-    { MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = GAP_TOGGLE} },
     TAGKEYS(                        XK_1,                      0)
     TAGKEYS(                        XK_2,                      1)
     TAGKEYS(                        XK_3,                      2)
